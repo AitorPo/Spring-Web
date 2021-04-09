048a3f9e469fb26d3ad452583abdc0ad0cab2062
AitorPo <aitorpoquetgin@gmail.com>
2021-04-09 19:17:54 +0200

    Falta terminar documentación

--- src/main/java/com/svalero/springweb/api_config/ShopConfig.java
+++ src/main/java/com/svalero/springweb/api_config/ShopConfig.java
@@ -10,6 +10,19 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @SecurityScheme(name = "bearerAuth",
@@ -19,15 +32,56 @@ import org.springframework.context.annotation.Configuration;
                 scheme = "bearer")
 public class ShopConfig {
 
-    @Bean
-    public OpenAPI myOpenAPI(){
-        return new OpenAPI()
-                .components(new Components())
-                .info(new Info().title("Aitor's API shop")
-                                .description("A_A 2ª Evaluación Acceso a datos")
-                                .contact(new Contact()
-                                        .name("Aitor Poquet")
-                                        .email("a24837@svalero.com"))
-                                .version("1.0"));
+   @Bean
+    public OpenAPI myOpenAPI() {
+       return new OpenAPI()
+               .components(new Components())
+               .info(new Info().title("Aitor's API shop")
+                       .description("A_A 2ª Evaluación Acceso a datos")
+                       .contact(new Contact()
+                               .name("Aitor Poquet")
+                               .email("a24837@svalero.com"))
+                       .version("1.0"));
+
+   }
+
+    /*@Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "My REST API",
+                "Some custom description of API.",
+                "1.0",
+                "Terms of service",
+                new springfox.documentation.service.Contact("Sallo Szrajbman", "www.baeldung.com", "salloszraj@gmail.com"),
+                "License of API",
+                "API license URL",
+                Collections.emptyList());
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+    }*/
+
 }
